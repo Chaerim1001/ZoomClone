25660a5f14d700b555bc5d108d7ca11afaafd124
ParkChaerim <qkrdbsk28@naver.com>
2022-02-16 02:47:30 +0900

    닉네임 설정 기능 추가

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,3 +1,12 @@
+const messageList = document.querySelector("ul");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
+
+
+function makeMessage(type, payload){
+    const msg = {type, payload};
+    return JSON.stringify(msg);
+}
 //app.js에서 socket은 서버로의 연결을 의미!
 const socket = new WebSocket(`ws://${window.location.host}`);
 
@@ -6,6 +15,9 @@ socket.addEventListener("open", () => {
 });
 
 socket.addEventListener("message", (message) => {
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
     console.log("New message:", message.data);
 });
 
@@ -13,6 +25,16 @@ socket.addEventListener("close", () => {
     console.log("Disconnected from Server ❌");
 });
 
-setTimeout(() => {
-    socket.send("hello from the browser!!!");
-}, 10000)
+messageForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(makeMessage("new_message", input.value));
+    input.value = "";
+});
+
+nickForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    socket.send(makeMessage("nickname", input.value));
+    input.value = "";
+});
--- src/server.js
+++ src/server.js
@@ -18,16 +18,34 @@ const server = http.createServer(app);
 //create webSocket server
 const wss = new WebSocket.Server({ server });
 
+const sockets = [];
+
 // on method는 socket에 연결된 사람의 정보를 제공해준다.
 // server.js에서 socket은 연결된 브라우저를 의미
 wss.on("connection", (socket) => {
+    sockets.push(socket);
+
+    socket["nickname"] = "Anon"; //익명 사용자를 위함
+
     console.log("Connected to Browser ✔️");
 
     socket.on("close", () => console.log("Disconnected from the Browser ❌"));
-    socket.on("message", (message) => {
-        console.log(message.toString('utf8'));
+
+    socket.on("message", (msg) => {
+        //string 형태로 받은 message를 JSON 형식으로 바꿔줘야 type을 확인해서 처리가 가능해진다
+        const message = JSON.parse(msg);
+
+        switch(message.type){
+            case "new_message":
+                sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));
+                break
+            case "nickname": 
+                socket["nickname"] = message.payload;
+                break
+        }
+ 
     });
-    socket.send("hello");
+
 });
 
 server.listen(3000, handleListen);
