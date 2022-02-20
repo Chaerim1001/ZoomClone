e0df58137df75cdb09f7f1a314513711fb0864a8
ParkChaerim <qkrdbsk28@naver.com>
2022-02-20 22:01:54 +0900

    카메라 on/off, 음소거 기능

--- src/public/js/app2.js
+++ src/public/js/app2.js
@@ -1,8 +1,12 @@
 const socket = io();
 
 const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
 
 let myStream; //stream: 비디오 + 오디오
+let muted = false;
+let cameraOff= false;
 
 
 async function getMedia(){
@@ -21,3 +25,34 @@ async function getMedia(){
 };
 
 getMedia();
+
+function handleMuteClick(){
+
+    myStream.getAudioTracks().forEach((track) => (track.enabled = !track.enabled));
+
+    if(!muted){
+        muteBtn.innerText = "Unmute";
+        muted = true;
+    }
+    else{
+        muteBtn.innerText = "mute";
+        muted = false;
+    }
+};
+
+function handleCameraClick(){
+
+    myStream.getVideoTracks().forEach((track) => (track.enabled = !track.enabled));
+    
+    if(cameraOff){
+        cameraBtn.innerText = "Turn Camera Off";
+        cameraOff = false;
+    }
+    else{
+        cameraBtn.innerText = "Turn Camera On";
+        cameraOff = true;
+    }
+};
+
+muteBtn.addEventListener("click", handleMuteClick); 
+cameraBtn.addEventListener("click", handleCameraClick);
