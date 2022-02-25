364591a948a383809be078957549e9874e70e1bd
ParkChaerim <qkrdbsk28@naver.com>
2022-02-25 22:46:33 +0900

    iceCandidate (다른 사용자 추가)

--- src/public/js/app2.js
+++ src/public/js/app2.js
@@ -140,21 +140,42 @@ socket.on("welcome", async () => {
 });
 
 //Peer B에서 실행되는 코드
-socket.on("offer",async (offer) => {
+socket.on("offer", async (offer) => {
+    console.log("received the offer");
     myPeerConnection.setRemoteDescription(offer); 
     const answer = await myPeerConnection.createAnswer();
     myPeerConnection.setLocalDescription(answer);
     socket.emit("answer", answer, roomName);
+    console.log("sent the answer");
   });
 
 
 socket.on("answer", (answer) => {
-myPeerConnection.setRemoteDescription(answer);
+    console.log("received the answer");
+    myPeerConnection.setRemoteDescription(answer);
 });
+
+socket.on("ice", (ice) => {
+    console.log("received candidate");
+    myPeerConnection.addIceCandidate(ice);
+  });
+
 // RTC code
 
 function makeConnection(){
     myPeerConnection = new RTCPeerConnection();
+    myPeerConnection.addEventListener("icecandidate", handleIce);
+    myPeerConnection.addEventListener("addstream", handleAddStream);  
     myStream.getTracks().forEach(
         (track) => myPeerConnection.addTrack(track, myStream));
+};
+
+function handleIce(data) {
+    console.log("sent candidate");
+    socket.emit("ice", data.candidate, roomName);
+};
+  
+function handleAddStream(data) {
+    const peerFace = document.getElementById("peerFace");
+    peerFace.srcObject = data.stream;
 };
